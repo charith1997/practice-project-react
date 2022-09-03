3c64a016f77011883a741912ef725996f0e15253
charith1997 <charithmadu97@gmail.com>
2022-09-03 20:11:02 +0530

    AddUser card.module.css file implemented

---
+++ src/components/User/AddUser.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
+
+const AddUser = () => {
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" />
+        <label htmlFor="age">Age (years)</label>
+        <input type="number" id="age" />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
+  );
+};
+
+export default AddUser;
